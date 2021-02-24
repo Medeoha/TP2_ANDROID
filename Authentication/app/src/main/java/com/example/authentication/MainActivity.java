70d62b7a90e81479ed5f01f9b7e8375d7140eaee
Medeoha <Medeoha@users.noreply.github.com>
2021-02-24 19:40:32 +0100

    Comentaires

--- Authentication/app/src/main/java/com/example/authentication/MainActivity.java
+++ Authentication/app/src/main/java/com/example/authentication/MainActivity.java
@@ -31,32 +31,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         buttonAuth = findViewById(R.id.buttonAuth);
-        buttonAuth.setOnClickListener(new View.OnClickListener() {
+        buttonAuth.setOnClickListener(new View.OnClickListener() {// On definit l'action au click du bouton 
             @Override
             public void onClick(View v) {
                 //((TextView)findViewById(R.id.EditName)).setText("Lapin");
                 EditText EName = findViewById(R.id.EditName);
                 EditText Epwd = findViewById(R.id.EditPwd);
-                ((TextView)findViewById(R.id.Resultext)).setText("le pain c'st bon -1");
-                Thread thread = new Thread(new Runnable() {
+                Thread thread = new Thread(new Runnable() {// on crée un nouveau Thread
                     @Override
                     public void run() {
-                        ((TextView)findViewById(R.id.Resultext)).setText("le pain c'st bon -2");
 
                         try {
                             URL url = null;
-                            url = new URL("https://httpbin.org/basic-auth/bob/sympa");
+                            url = new URL("https://httpbin.org/basic-auth/bob/sympa");//On crée une nouvelle URL avec le lien donné dans le TP
 
                             String basicAuth = "Basic " + Base64.encodeToString((EName.getText()+":"+Epwd.getText()).getBytes(), Base64.NO_WRAP);
                             //String basicAuth = "Basic " + Base64.encodeToString("bob:sympa".getBytes(),                             Base64.NO_WRAP);
                             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                             urlConnection.setRequestProperty ("Authorization", basicAuth);
                             //urlConnection.setUseCaches(false);
-                            urlConnection.connect();
+                            urlConnection.connect();// on etablit la connexion avec l'url
 
                             try {
-                                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                                String s = readStream(in);
+                                InputStream in = new BufferedInputStream(urlConnection.getInputStream());//on recupere le Steam
+                                String s = readStream(in);// On  convertie ce steam en String
 
                                 Log.i("JFL", s);
                                 runThread("My result here:\n" + s);
@@ -70,18 +68,17 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
-                thread.start();
+                thread.start();// on lance ce nouveau Thread
             }
         });
 
     }
-    private void runThread(String msg) {
+    private void runThread(String msg) {//Fonction permettant d'actualiser le ResultText en dehors du thread principal
 
         new Thread() {
             public void run() {
 
                 runOnUiThread(new Runnable() {
-
                     @Override
                     public void run() {
                         ((TextView)findViewById(R.id.Resultext)).setText(msg);
@@ -91,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }.start();
     }
-    private String readStream(InputStream is) throws IOException {
+    private String readStream(InputStream is) throws IOException {// On prend le stream on le parse et on en le met en String
         StringBuilder sb = new StringBuilder();
         BufferedReader r = new BufferedReader(new InputStreamReader(is),1000);
         for (String line = r.readLine(); line != null; line =r.readLine()){
